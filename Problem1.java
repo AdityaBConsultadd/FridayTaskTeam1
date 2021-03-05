02a2c13d945e124e331486d3c6f92e9eb74d8449
vaibhav shah <vaibhavshah96@gmail.com>
2021-03-05 17:39:37 -0500

    Added problem 1 and 2.

--- Problem1.java
+++ Problem1.java
@@ -7,7 +7,29 @@ class  Problem1{
         res= String.valueOf(value);
 
         System.out.println("Sum of two non-Negative String number is: "+ res);
+        
+        
+        
+        //Vaibhav
+        Vaibhav sumOfString = new Vaibhav("345", "789");
+		System.out.println(sumOfString.sum());
     }
 
+
+}
+
+class Vaibhav {
+	String str1 = new String();
+	String str2 = new String();
+
+	Vaibhav(String string1, String string2) {
+		this.str1 = string1;
+		this.str2 = string2;
+	}
+
+	int sum() {
+		int sum = Integer.parseInt(str1) + Integer.parseInt(str2);
+		return sum;
+	}
 
 }
