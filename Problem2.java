928095edd8c16720f774730f8ac7aef21f5a464c
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-03-05 15:56:24 -0600

    q1. Problem1

--- Problem2.java
+++ Problem2.java
@@ -1,5 +1,27 @@
-class Problem2{
+import java.util.Arrays;
+
+class Problem2 {
     public static void main(String[] args) {
-        
+
+        char[] ch = {'a', 'a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'c'};
+        System.out.print(Arrays.toString(ch) + " = ");
+        int[] freq = new int[ch.length];
+        int i;
+        String str = "";
+        for (i = 0; i < ch.length; i++) {
+            freq[i] = 1;
+            for (int j = i + 1; j < ch.length; j++) {
+                if (ch[i] == ch[j]) {
+                    freq[i]++;
+                    ch[j] = '0';
+                }
+            }
+        }
+        for (i = 0; i < freq.length; i++) {
+            if (ch[i] != ' ' && ch[i] != '0')
+                System.out.print(ch[i] + "" + freq[i]);
+        }
+
     }
+
 }
