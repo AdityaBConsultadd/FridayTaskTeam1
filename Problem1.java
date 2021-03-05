2ef3ed3dfaae5e771e745000921f9cc40c3e60ce
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-03-05 15:08:29 -0600

    q1. Problem1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,13 @@
+class  Problem1{
+    public static void main(String[] args) {
+        String num1 = "123";
+        String num2 = "122";
+        String res;
+        int value = Integer.parseInt(num1) + Integer.parseInt(num2);
+        res= String.valueOf(value);
 
+        System.out.println("Sum of two non-Negative String number is: "+ res);
+    }
+
+
+}
